be5c232dc9c372d03de3128e65782543acd7764d
Manpreet-singh <ms4110415@gmail.com>
2020-06-04 10:43:35 +0530

    popup update

--- js/script.js
+++ js/script.js
@@ -18,5 +18,23 @@ function clck(){
     demo1.style.display="flex";
     demo2.style.display="flex";
   }
- 
 }
+
+var inptsubmit=document.querySelector('#select');
+var inpttxt=document.querySelector('#subs');
+var popup=document.querySelector('#popup');
+var content=document.querySelector('#content');
+inptsubmit.addEventListener('click',function(){
+  // alert(inpttxt.value);
+  if(inpttxt.value==""){
+    alert("Please write something here");
+  }else{
+  popup.style.display="flex";
+  content.innerHTML=inpttxt.value;
+  inpttxt.value="";
+  setTimeout(() => {
+    popup.style.display="none";
+  }, 1000);
+}
+})
+console.log(inpttxt.value);
